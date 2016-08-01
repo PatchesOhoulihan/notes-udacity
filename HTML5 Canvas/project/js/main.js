8371004bb9317c3b42f5d0820ebb74ccb834d2fd
Schmalte <schmalte@xyz.com>
2016-08-01 16:06:32 +0200

    FEATURE: Stuff

--- HTML5 Canvas/project/js/main.js
+++ HTML5 Canvas/project/js/main.js
@@ -17,15 +17,25 @@
   canvasContext.fillRect(100,100,100,100);
   canvasContext.strokeRect(50,50,50,50);
 
+  canvasContext.strokeStyle = "#33cc33";
+
   canvasContext.beginPath();
   canvasContext.moveTo(10,10);
   canvasContext.lineTo(50,50);
   canvasContext.lineTo(50,10);
   canvasContext.lineTo(10,10);
 
+  //REMEMBER: The results of .fillStyle value show up because the call of .fill() after you apply it.
+  canvasContext.fillStyle ="Blue";
   canvasContext.fill();
 
+  canvasContext.fillStyle = "ghostwhite";
+  canvasContext.strokeStyle = "black";
+  canvasContext.lineWidth=5;
+  canvasContext.font = "700 36pt impact"
   canvasContext.strokeRect(5,5,55,55);
+  canvasContext.strokeText("Meme Generator", 50, 50);
+  canvasContext.fillText("Meme Generator", 50, 50);
 
 
 
