4d96156892854959010988378a67f909b9971587
Schmalte <schmalte@xyz.com>
2016-08-03 17:19:21 +0200

    FEATURE: Start HTML 5 Course

---
+++ HTML5 Games Development/project1/js/main.js
@@ -0,0 +1,14 @@
+(() =>(
+
+let $ = document.querySelector.bind(document);
+let $$ = document.querySelectorAll.bind(document);
+
+let setup = () => {
+  let body = $('body');
+  let canvas = document.createElement('canvas');
+
+  canvas.width = '1200px';
+  canvas.height = "720px"; 
+}
+
+))();
